4d90d7b02c1318e4aa8220e4ed418c43696fbc9c
TimYuen <tyuen007@gmail.com>
2023-06-26 21:41:07 -0700

    hello world

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\Tim\Documents\SleepiCat\Rust101\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\Tim\Documents\SleepiCat\Rust101\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\Tim\Documents\SleepiCat\Rust101\hello\target\debug\hello.exe: C:\Users\Tim\Documents\SleepiCat\Rust101\hello\src\main.rs
